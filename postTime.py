b47c571e5bfb791076bf8667b62d5f4b06d04fb2
2eD <68163789+2edcovid@users.noreply.github.com>
2020-11-01 10:20:57 -0600

    Update postTime.py

--- postTime.py
+++ postTime.py
@@ -4,7 +4,7 @@ def shouldPost():
   post = False
   current_time = time.strftime("%H:%M:%S")
   start = '15:00:00'
-  end = '17:00:00'
+  end = '18:00:00'
   if current_time > start and current_time < end:
     post = True
   else:
